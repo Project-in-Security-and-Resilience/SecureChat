4cb1d114f8e008ae979bbbe7581f57a5139a1857
Harry <874497521@qq.com>
2024-03-14 05:02:15 +0000

    create customized account automatically(for ai assistant)

---
+++ src/ai_helper/GenUniAcc.js
@@ -0,0 +1,45 @@
+import {db, auth} from "./MyFirebase.js";
+import {collection, doc, setDoc, query, where, getDocs} from "firebase/firestore";
+import {createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
+import {gptAccountInfos} from "./UNIQUE_GPT_ACCOUNT.js";
+
+
+// Find the account, then check the account if is unique
+const q = query(collection(db, "users"), where("email", "==", gptAccountInfos.email));
+const res = await getDocs(q);
+console.log(res.size === 0)
+
+// not create the unique GPT account, need to create an account (the account of GPT is only one)
+if (res.size === 0) {
+    try {
+        //Create user
+        const res = await createUserWithEmailAndPassword(auth, gptAccountInfos.email, gptAccountInfos.password);
+
+        try {
+            //Update profile
+            await updateProfile(res.user, {
+                displayName: gptAccountInfos.displayName,
+                photoURL: gptAccountInfos.photoURL,
+            });
+            //create unique gpt account on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+                uid: res.user.uid,
+                displayName: gptAccountInfos.displayName,
+                email: gptAccountInfos.email,
+                photoURL: gptAccountInfos.photoURL,
+            });
+            // console.log("done1");
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            // console.log("done2");
+        } catch (err) {
+            //todo transaction， rollback manually
+            console.log(err);
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+} else {
+    // do nothing
+}
---
+++ src/ai_helper/UNIQUE_GPT_ACCOUNT.js
@@ -0,0 +1,7 @@
+// GPT account information, todo change the info
+export const gptAccountInfos = {
+  displayName: "testing",
+  email: "testing1@gmail.com",
+  password: "testing",
+  photoURL: "https://th.bing.com/th/id/OIP.bScrk0MBC5ApB9SrTMUkugAAAA?rs=1&pid=ImgDetMain"
+}
