1991dfffdd721bd58ee38f6bfffd8a7909743d19
Harry <874497521@qq.com>
2024-03-16 15:15:56 +0000

    logout then refresh web page to fix the bug of cache issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,17 @@ import DisplayKey from "./DisplayKey"
 const Navbar = () => {
   const {currentUser} = useContext(AuthContext)
 
+  const refresh = async ()=>{
+      location.reload();
+  }
+
   return (
     <div className='navbar'>
       <span className="logo">SexureXChat</span>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
-        <button onClick={()=>signOut(auth)}>logout</button>
+        <button onClick={()=>signOut(auth)} onMouseUp={refresh}>logout</button>
         <DisplayKey />
       </div>
     </div>
