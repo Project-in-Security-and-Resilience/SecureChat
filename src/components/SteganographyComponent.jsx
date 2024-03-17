e4eaac979f9861de3cf6facba6bb9fda078f6636
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2024-03-17 05:26:55 +0000

    Added a download button to allow users downloaded encoded message easily

--- src/components/SteganographyComponent.jsx
+++ src/components/SteganographyComponent.jsx
@@ -34,7 +34,15 @@ const SteganographyComponent = () => {
 
     reader.readAsDataURL(file);
   }
-
+  // Function to trigger the download
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = encodedImage; // Set the href to the data URL of the encoded image
+    link.download = 'encoded-image.png'; // Set the default filename for the download
+    document.body.appendChild(link); // Append to the body
+    link.click(); // Trigger the click to download
+    document.body.removeChild(link); // Remove the link from the body
+  };
   // Handler for source image file input
   const handleSourceImageChange = (event) => {
     readURL(event.target.files[0]);
@@ -65,16 +73,21 @@ const SteganographyComponent = () => {
         Hide Message Into Image
       </button>
 
+      <div className="imageContainer">
       <div className="img-cont">
         <div className="img1">
-          <h5>Source Image</h5>
-          <img src={sourceImage} alt="Source" />
+          <h5 className="sourcePic">Source Image</h5>
+          <img className="sourcePic" src={sourceImage} alt="Source"/>
         </div>
 
         <div>
-          <h5>Message Encoded Image</h5>
-          <img src={encodedImage} alt="Encoded" />
+          <h5 className="encodedText">Encoded Image</h5>
+          <img className="encodedPic" src={encodedImage} alt="Encoded"/>
         </div>
+        <button onClick={handleDownload} className="downloadButton">Download</button>
+                
+              
+      </div>
       </div>
 
       <div>
@@ -86,8 +99,8 @@ const SteganographyComponent = () => {
           onChange={handleEncodedImageChange}
         />
         <div>
-          <h5>Decoded Text:</h5>
-          <h2>{decodedText}</h2>
+          <h5 className="decodedText">Decoded Text:</h5>
+          <h2 className="decodedText"> {decodedText}</h2>
         </div>
       </div>
     </div>
