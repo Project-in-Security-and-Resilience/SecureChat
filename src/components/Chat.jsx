be00823de8ef911ebbfb2e689494bba77ef053da
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2024-03-17 05:28:04 +0000

    Change path from new page to  pop up

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,23 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext , useState } from "react";
 import Cam from "../img/cam.png";
 import Add from "../img/add.png";
 import More from "../img/more.png";
 import Messages from "./Messages";
 import Input from "./Input";
 import { ChatContext } from "../context/ChatContext";
-import { useNavigate, Link} from 'react-router-dom';
 import SteganographyComponent from "./SteganographyComponent";
 
 
 const Chat = () => {
   const { data } = useContext(ChatContext);
-  const navigate = useNavigate();
+  const [isStegModalOpen, setIsStegModalOpen] = useState(false);
 
-    // Function to handle navigation
-    const goToPage = () => {
-      navigate('/SteganographyComponent'); 
-      
-    };
+
+  const toggleStegModal = () => {
+    setIsStegModalOpen(!isStegModalOpen);
+  };
+
 
   return (
     <div className="chat">
@@ -29,7 +28,17 @@ const Chat = () => {
           <img src={More} alt="" />
         </div>
       </div>
-      <button onClick={goToPage}>Send/Decode hidden message</button>
+      <button onClick={toggleStegModal}>Send/Decode hidden message</button>
+      {isStegModalOpen && (
+        <div className="modalBackdrop" onClick={toggleStegModal}>
+          <div className="steganographyModal" onClick={e => e.stopPropagation()}>
+            <SteganographyComponent />
+            <button className="closeModalButton" onClick={toggleStegModal}>Close</button>
+          </div>
+        </div>
+        
+      )}
+      
       <Messages />
       <Input/>
     </div>
