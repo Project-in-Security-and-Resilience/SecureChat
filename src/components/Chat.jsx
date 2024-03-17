b30892f8c668bea6c64abe94ac7f3915b7c98d1c
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2024-03-17 02:04:02 +0000

    call the modified app to iclude router to steganograp hyComponent as a new page

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,9 +5,19 @@ import More from "../img/more.png";
 import Messages from "./Messages";
 import Input from "./Input";
 import { ChatContext } from "../context/ChatContext";
+import { useNavigate, Link} from 'react-router-dom';
+import SteganographyComponent from "./SteganographyComponent";
+
 
 const Chat = () => {
   const { data } = useContext(ChatContext);
+  const navigate = useNavigate();
+
+    // Function to handle navigation
+    const goToPage = () => {
+      navigate('/SteganographyComponent'); 
+      
+    };
 
   return (
     <div className="chat">
@@ -19,6 +29,7 @@ const Chat = () => {
           <img src={More} alt="" />
         </div>
       </div>
+      <button onClick={goToPage}>Send/Decode hidden message</button>
       <Messages />
       <Input/>
     </div>
