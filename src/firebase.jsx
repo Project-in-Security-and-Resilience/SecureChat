56cbfbf0659cf2885aa672780452433d1646a011
Tushar Laad <tusharlaad2002@gmail.com>
2024-03-22 23:41:16 +0000

    performance init

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,6 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+import { getPerformance } from "firebase/performance";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,3 +26,5 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore()
+// Initialize Performance Monitoring and get a reference to the service
+const perf = getPerformance(app);
