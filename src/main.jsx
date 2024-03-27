30b229663a2056c76468e06d31ebe7153312f522
Tushar Laad <tusharlaad2002@gmail.com>
2024-03-27 08:21:59 +0000

    remove vercel speed

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,7 +10,6 @@ root.render(
     <ChatContextProvider>
       <React.StrictMode>
         <App />
-        <SpeedInsights />
       </React.StrictMode>
     </ChatContextProvider>
   </AuthContextProvider>
